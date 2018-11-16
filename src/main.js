520c2391ed9c2adaa43b5934035037bdf3a5daa6
yangzg <yangzg@USER-20180421QO>
2018-11-16 10:56:37 +0800

    资源更新

--- src/main.js
+++ src/main.js
@@ -9,24 +9,25 @@ const requireComponent = require.context(
   /\w+\.(vue|js)$/
 )
 requireComponent.keys().forEach(fileName => {
-  // 获取组件配置
-  const componentConfig = requireComponent(fileName)
+ 
+  if (run) {
+    // 获取组件配置
+    const componentConfig = requireComponent(fileName)
 
-  // 获取组件的 PascalCase 命名
-  const componentName = upperFirst(
-    camelCase(
-      // 剥去文件名开头的 `./` 和结尾的扩展名
-      fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
+    // 获取组件的 PascalCase 命名
+    const componentName = upperFirst(
+      camelCase(
+        // 剥去文件名开头的 `./` 和结尾的扩展名
+        fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
+      )
     )
-  )
-  
-  // 全局注册组件
-  Vue.component(
-    componentName,
-    // 如果这个组件选项是通过 `export default` 导出的，
-    // 那么就会优先使用 `.default`，
-    // 否则回退到使用模块的根。
-    componentConfig.default || componentConfig
-  )
+    // 全局注册组件
+    Vue.component(
+      componentName,
+      // 如果这个组件选项是通过 `export default` 导出的，
+      // 那么就会优先使用 `.default`，
+      // 否则回退到使用模块的根。
+      componentConfig.default || componentConfig
+    )
+  }
 })
-
---
+++ src/main2.js
@@ -0,0 +1,5 @@
+import resList from './view/resList.vue'
+// import supermap from './view/supermap.vue';
+
+Vue.component('res-list', resList);
+// Vue.component('supermap', supermap);
